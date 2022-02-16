af7359b49d8af8e5b11f786276e51fcce0a3d96a
KP <kyleprock@verizon.net>
2022-02-16 09:47:32 -0600

    init commit

---
+++ app/models/article.rb
@@ -0,0 +1,9 @@
+class Article < ApplicationRecord
+    include Visible
+
+    has_many :comments, dependant: :destroy
+    
+    validates :title, presence: true
+    validates :body, presence: true, length: { minimum: 10 }
+
+end
